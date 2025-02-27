c9c7d8ab500036e94ca84d22e37a8a75b233036e
misterD05 <sebastiano.michi@gmail.com>
2025-02-27 13:44:26 +0100

    prova 2

--- scripts/general.js
+++ scripts/general.js
@@ -1,9 +1,21 @@
-import Enviroment from "./enviroment.js";
 const env = new Enviroment(100,100);
 
 var canva = document.getElementById("enviroment");
 var context = canva.getContext("2d");
 
 
-context.fillStyle = "#FF0000";
-context.fillRect(0,0,20,20);
+function drawEnv(width, heigth, widtCell, heigthCell){
+    
+    context.lineWidth = 1;
+    for (let x = 0; x < width; x += 10) {
+        for (let y = 0; y < heigth; y += 10) {
+            context.strokeRect(x,y,30,30);
+        }
+    }
+}
+
+
+console.log(env.width);
+
+drawEnv( env.width, env.heigth, window.width/env.width, window.height/env.height);
+
