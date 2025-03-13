3426aee82f3265a4f54c2e76b4717a351089c546
misterD05 <157386570+misterD05@users.noreply.github.com>
2025-03-13 13:33:55 +0100

    tenth bis commit

--- scripts/general.js
+++ scripts/general.js
@@ -1,6 +1,6 @@
 const env = new Enviroment(100,100);
 console.log("ciao")
-let enviroment = document.getElementsById("environment");
+let enviroment = document.getElementById('environment')
 enviroment.style.display = "grid";
 let box = document.createElement('div');
 box.setAttribute('class', 'box');
