4a0f34e40fb2183feeee841dc3e883e005715aa0
misterD05 <157386570+misterD05@users.noreply.github.com>
2025-03-13 13:43:15 +0100

    twelveth commit

--- scripts/general.js
+++ scripts/general.js
@@ -7,8 +7,11 @@ box.setAttribute('class', 'box');
 box.style.border = "solid 2px black";
 
 for (let index = 0; index < env.heigth; index++) {
-    box.style.heigth = "" + enviroment.heigth/env.heigth + "px";
+    
     box.style.width = "auto";
     enviroment.appendChild(box);
     console.log(index);
 }
+
+
+document.getElementsByClassName("box");
