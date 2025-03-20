88717b7b08322968e46083008227796967228fc0
misterD05 <157386570+misterD05@users.noreply.github.com>
2025-03-20 13:29:57 +0100

    try grid

--- scripts/general.js
+++ scripts/general.js
@@ -7,9 +7,10 @@ let enviroment = document.getElementById('environment')
 enviroment.style.display = "grid";
 enviroment.style.gridTemplateColumns =  "auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto ";
 
-for (let index = 0; index < env.heigth ; index++) {
+for (let index = 0; index < env.heigth * env.width ; index++) {
     let box = document.createElement('div');
     box.setAttribute('class', 'box');
+    box.style.background = "#F8FFE5"
     box.style.border = "solid 2px black";
     console.log(index);
     box.style.width = "auto";
