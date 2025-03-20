d2b5aa6589e4143abb279fc0da47618a068dded7
misterD05 <157386570+misterD05@users.noreply.github.com>
2025-03-20 13:15:32 +0100

    try grid

--- scripts/general.js
+++ scripts/general.js
@@ -5,14 +5,11 @@ let y = 100;
 const env = new Enviroment(parseInt(x),parseInt(y));
 let enviroment = document.getElementById('environment')
 enviroment.style.display = "grid";
-let box = document.createElement('div');
-box.setAttribute('class', 'box');
-box.style.border = "solid 2px black";
-console.log(String(env.heigth));
-console.log(parseInt(env.heigth));
 
 for (let index = 0; index < env.heigth ; index++) {
-    
+    let box = document.createElement('div');
+    box.setAttribute('class', 'box');
+    box.style.border = "solid 2px black";
     console.log(index);
     box.style.width = "auto";
     enviroment.appendChild(box);
