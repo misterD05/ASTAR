ac0fe15707692ba8dd9e8de1ae1c08863d01d094
misterD05 <157386570+misterD05@users.noreply.github.com>
2025-03-27 12:40:58 +0100

    grid

--- scripts/general.js
+++ scripts/general.js
@@ -1,13 +1,13 @@
-let x = 25;
-let y = 25;
+let x = 10;
+let y = 10;
 
 
 const env = new Enviroment(parseInt(x),parseInt(y));
 let enviroment = document.getElementById('environment')
 enviroment.style.display = "grid";
-enviroment.style.gridTemplateColumns =  "auto auto auto auto auto";// auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto ";
+enviroment.style.gridTemplateColumns =  "auto auto auto auto auto auto auto auto auto auto";// auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto ";
 
-for (let index = 0; index < env.heigth; index++) {
+for (let index = 0; index < env.heigth * env.width; index++) {
     let box = document.createElement('div');
     box.setAttribute('class', 'box');
     box.style.background = "#F8FFE5"
@@ -19,5 +19,6 @@ for (let index = 0; index < env.heigth; index++) {
 
 const bot = new Automaton(0,0,env, new State(25,25,1,0,null));
 const arrGrid = document.getElementsByClassName('box');
+
 
 console.log(document.getElementsByClassName("box")[0].offsetHeigth());
